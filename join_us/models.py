658a7bd8e740ca5983e7505561e1dacad358669e
AmanAg513 <amanagrawal513@gmail.com>
2017-06-22 09:13:22 +0530

    join us updated

--- join_us/models.py
+++ join_us/models.py
@@ -11,5 +11,6 @@ class JoinUsData(models.Model):
     visibility = models.BooleanField(default=True)
     date = models.DateField(auto_now_add=True,null=True)
     time = models.TimeField(auto_now_add=True,null=True)
+    description = models.TextField(null=True,blank=True)
 
     
