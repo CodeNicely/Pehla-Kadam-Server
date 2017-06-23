62447ad39d86ffbb354a305e2ed9a69994e73610
Anurag Thakur <anuragt0007@gmail.com>
2017-06-23 19:34:41 +0530

    Update settings.py

--- pehlakadam/settings.py
+++ pehlakadam/settings.py
@@ -91,13 +91,22 @@ WSGI_APPLICATION = 'pehlakadam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pehlakadamdb',
+        'USER': 'pehla_kadam_admin',
+        'PASSWORD': 'Password@123',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
